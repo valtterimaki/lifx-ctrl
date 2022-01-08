db02b7cf225308cc91b67136e532b4a69a701538
Valtteri Mäki <valtteri@taiste.fi>
2022-01-08 13:13:44 +0200

    basic controls working

--- lifx-ctrl.py
+++ lifx-ctrl.py
@@ -8,15 +8,17 @@ from pynput import keyboard
 
 global strip
 
-# state variables
-state_power = 0
+# set state variables
+state_power = 1
 state_zonemode = 0
 state_colormode = 0
-state_switch_temperature = 1
+state_switch_brightness = 0
 state_switch_color = 0
 state_preset = 0
 
-ph_input_pot = 0.0 # TODO For now, i'm assuming the pot range as 0-1. Correct this as the actual connections are made.
+# set pot simulation initial value
+ph_input_pot = 0.5 # TODO For now, i'm assuming the pot range as 0-1. Correct this as the actual connections are made.
+ph_input_pot_prev = ph_input_pot
 
 # Keyboard input placeholder to simulate GPIO inputs
 # TODO replace with actual gpio stuff
@@ -30,22 +32,58 @@ def on_press(key):
 
 def on_release(key):
 
-    # placeholder variable to simulate pot reading
-    global ph_input_pot
+  # declare state variables (here because wtf...)
+  global state_power
+  global state_zonemode
+  global state_colormode
+  global state_switch_brightness
+  global state_switch_color
+  global state_preset
 
-    #print('{0} released'.format(key))
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False
-    if 'char' in dir(key):
-      if key.char == 'q':
-        strip.set_power("on", True)
-        state_power = 1
-        print("power " + str(state_power))
-      if key.char == 'a':
-        strip.set_power("off", True)
-        state_power = 0
-        print("power " + str(state_power))
+  # placeholder variable to simulate pot reading
+  global ph_input_pot
+  global ph_input_pot_prev
+
+  #print('{0} released'.format(key))
+  #if key == keyboard.Key.esc:
+  #    # Stop listener
+  #    return False
+  if 'char' in dir(key):
+    if key.char == 'q':
+      strip.set_power("on", True)
+      state_power = 1
+      print("power " + str(state_power))
+    if key.char == 'a':
+      strip.set_power("off", True)
+      state_power = 0
+      print("power " + str(state_power))
+    if key.char == 'h':
+      state_colormode = 1 - state_colormode
+      print("colormode " + str(state_colormode))
+    if key.char == 'j':
+      state_switch_color = 1
+      state_switch_brightness = 0
+      print("color switch on")
+    if key.char == 'k':
+      state_switch_color = 0
+      state_switch_brightness = 0
+      print("saturation switch on")
+    if key.char == 'l':
+      state_switch_color = 0
+      state_switch_brightness = 1
+      print("brightness switch on")
+
+#-------------
+
+    ## first of all, checking if lifx power is on or not
+
+    # if power is off, only power on switch function and indicator lights are active
+    if state_power == 0:
+      pass
+
+    # if power is on, all functions are active
+    if state_power == 1:
+
       if key.char == 'p':
         if ph_input_pot < 0.95:
           ph_input_pot += 0.1
@@ -54,6 +92,39 @@ def on_release(key):
         if ph_input_pot > 0.05:
           ph_input_pot -= 0.1
           print("pot value " + str(ph_input_pot))
+
+      ## checking if strip is in zone mode or default mode and determine the behaviour of the controls in both
+
+      # if zone mode is OFF
+      if state_zonemode == 0:
+
+        # if the color mode is off only temperature is adjusted
+        if state_colormode == 0 and ph_input_pot != ph_input_pot_prev:
+          strip.set_colortemp(map(ph_input_pot, (0.0, 1.0), (2500, 9000)), 0, False)
+          ph_input_pot_prev = ph_input_pot
+
+        # if the color mode is on, the 3-way switch selects which parameter is changed
+        else:
+          # if brightness mode
+          if state_switch_brightness == 1 and ph_input_pot != ph_input_pot_prev:
+            strip.set_brightness(65535 * ph_input_pot, 0, False)
+            ph_input_pot_prev = ph_input_pot
+          # if color mode
+          elif state_switch_color == 1 and ph_input_pot != ph_input_pot_prev:
+            strip.set_hue(65535 * ph_input_pot, 0, False)
+            ph_input_pot_prev = ph_input_pot
+          # if saturation mode
+          elif state_switch_brightness == 0 and state_switch_color == 0 and ph_input_pot != ph_input_pot_prev:
+            strip.set_saturation(65535 * ph_input_pot, 0, False)
+            ph_input_pot_prev = ph_input_pot
+
+        # TODO encoder behaviour here (randomizer)
+
+      # if zone mode is ON
+      else:
+        # TODO knob behaviour here
+        # TODO encoder behaviour here
+        pass
 
 def map(val, src, dst):
     # Map the given value from the scale of src to the scale of dst.
@@ -108,11 +179,13 @@ def main():
   # check the power and set the state variable accordingly
   if strip.get_power() == 65535:
     state_power = 1
+    print("powertest")
+    print(state_power)
+
 
   #######################################
   #### actual running program itself ####
   #######################################
-
 
   while True:
 
@@ -122,44 +195,7 @@ def main():
         on_release=on_release) as listener:
       listener.join()
 
-    ## first of all, checking if lifx power is on or not
 
-    # if power is off, only power on switch function and indicator lights are active
-    if state_power == 0:
-      pass
-
-    # if power is on, all functions are active
-    if state_power == 1:
-
-      ## checking if strip is in zone mode or default mode and determine the behaviour of the controls in both
-
-      # if zone mode is OFF
-      if state_zonemode == 0:
-
-        # if the color mode is off only brightness is adjusted
-        if state_colormode == 0:
-          strip.set_brightness(65535 * ph_input_pot, 0, False)
-          print("test")
-
-        # if the color mode is on, the 3-way switch selects which parameter is changed
-        else:
-          # if temperature mode
-          if state_switch_temperature == 1:
-            strip.set_colortemp(map(ph_input_pot, (0.0, 1.0), (2500, 9000)), 0, False)
-          # if color mode
-          elif state_switch_color == 1:
-            strip.set_hue(65535 * ph_input_pot, 0, False)
-          # if saturation mode
-          else:
-            strip.set_saturation(65535 * ph_input_pot, 0, False)
-
-        # TODO encoder behaviour here (randomizer)
-
-      # if zone mode is ON
-      else:
-        # TODO knob behaviour here
-        # TODO encoder behaviour here
-        pass
 
 
       # TODO preset button behaviour
