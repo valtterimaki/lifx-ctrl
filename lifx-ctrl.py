6376d3e0bc705b3644b58c97e46cb46b0aabf727
Valtteri Mäki <valtteri@taiste.fi>
2022-01-22 22:17:31 +0200

    working code before soldering

--- lifx-ctrl.py
+++ lifx-ctrl.py
@@ -10,7 +10,7 @@ import digitalio
 import board
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
-
+from encoder import Encoder
 
 # GPIO library, note that the except part is for enabling dummy development on mac/pc
 try:
@@ -22,7 +22,6 @@ except:
 # from encoder import Encoder
 
 
-
 #####################
 ##### VARIABLES #####
 #####################
@@ -30,10 +29,9 @@ except:
 global strip
 
 # define buttons (TODO remap)
-SWITCH_POWER_ON = 1
-SWITCH_POWER_OFF = 2
-BTN_ZONE = 3
-BTN_COLOR = 4
+SWITCH_POWER = 27
+BTN_ZONE = 4
+BTN_COLOR = 17
 SWITCH_BRIGHTNESS = 5
 SWITCH_COLOR = 6
 
@@ -53,11 +51,11 @@ zone_set_color = [0, 0, 0, 5000]
 # create the spi bus
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 # create the cs (chip select)
-cs = digitalio.DigitalInOut(board.D5)
+cs = digitalio.DigitalInOut(board.D22)
 # create the mcp object
 mcp = MCP.MCP3008(spi, cs)
 # create an analog input channel on pin 0
-chan = AnalogIn(mcp, MCP.P0)
+chan0 = AnalogIn(mcp, MCP.P0)
 
 
 
@@ -225,44 +223,45 @@ def key_release(key):
 ########################################################################
 
 def btn_power_on_cb(channel):
-  print("Power switch on!")
   global state_power
-  strip.set_power("on", True)
-  state_power = 1
-  print("power " + str(state_power))
-
-
-def btn_power_off_cb(channel):
-  print("Power switch off!")
-  strip.set_power("off", True)
-  state_power = 0
-  print("power " + str(state_power))
+  sleep(0.009)
+  if GPIO.input(SWITCH_POWER) == 1:
+    print("Power switch on!")
+    strip.set_power("on", True)
+    state_power = 1
+    print("power " + str(state_power))
+  else:
+    print("Power switch off!")
+    strip.set_power("off", True)
+    state_power = 0
+    print("power " + str(state_power))
 
 
 def btn_zonemode_cb(channel):
-  print("Zonemode button pressed!")
-  global state_zonemode
-  global zone_set_color
+    print("Zonemode button pressed!")
+    global selected_zone
+    global state_zonemode
+    global zone_set_color
 
-  # first of all, check if lifx power is on or not and do nothing if not
-  if state_power == 1:
-    selected_zone = 0
-    if state_zonemode == 0:
-      zone_set_color = list(strip.get_color_zones(selected_zone, selected_zone + 1)[0])
-      temp_color = zone_set_color
-    sleep(0.1)
-    state_zonemode = 1 - state_zonemode
-    print("zonemode " + str(state_zonemode))
+    # first of all, check if lifx power is on or not and do nothing if not
+    if state_power == 1 and GPIO.input(BTN_ZONE) == 1:
+        selected_zone = 0
+        if state_zonemode == 0:
+            zone_set_color = list(strip.get_color_zones(selected_zone, selected_zone + 1)[0])
+            temp_color = zone_set_color
+        sleep(0.1)
+        state_zonemode = 1 - state_zonemode
+        print("zonemode " + str(state_zonemode))
 
 
 def btn_colormode_cb(channel):
-  print("Colormode button pressed!")
-  global state_colormode
+    print("Colormode button pressed!")
+    global state_colormode
 
-  # first of all, check if lifx power is on or not and do nothing if not
-  if state_power == 1:
-    state_colormode = 1 - state_colormode
-    print("colormode " + str(state_colormode))
+    # first of all, check if lifx power is on or not and do nothing if not
+    if state_power == 1 and GPIO.input(BTN_COLOR) == 1:
+        state_colormode = 1 - state_colormode
+        print("colormode " + str(state_colormode))
 
 
 def enc_cb(value, direction):
@@ -372,20 +371,22 @@ def main():
 
   GPIO.setmode(GPIO.BCM)
 
-  GPIO.setup(SWITCH_POWER_ON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-  GPIO.setup(SWITCH_POWER_OFF, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-  GPIO.setup(BTN_ZONE, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-  GPIO.setup(BTN_COLOR, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-  GPIO.setup(SWITCH_BRIGHTNESS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-  GPIO.setup(SWITCH_COLOR, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+  GPIO.setup(SWITCH_POWER, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+  GPIO.setup(BTN_ZONE, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+  GPIO.setup(BTN_COLOR, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+  GPIO.setup(SWITCH_BRIGHTNESS, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+  GPIO.setup(SWITCH_COLOR, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-  GPIO.add_event_detect(SWITCH_POWER_ON, GPIO.FALLING, callback=btn_power_on_cb, bouncetime=50)
-  GPIO.add_event_detect(SWITCH_POWER_OFF, GPIO.FALLING, callback=btn_power_off_cb, bouncetime=50)
-  GPIO.add_event_detect(BTN_ZONE, GPIO.FALLING, callback=btn_zonemode_cb, bouncetime=50)
-  GPIO.add_event_detect(BTN_COLOR, GPIO.FALLING, callback=btn_colormode_cb, bouncetime=50)
+  GPIO.add_event_detect(SWITCH_POWER, GPIO.BOTH, callback=btn_power_on_cb, bouncetime=10)
+  GPIO.add_event_detect(BTN_ZONE, GPIO.RISING, callback=btn_zonemode_cb, bouncetime=10)
+  GPIO.add_event_detect(BTN_COLOR, GPIO.RISING, callback=btn_colormode_cb, bouncetime=10)
 
-  # enc = Encoder(6, 7, enc_cb) # remap gpio
+  enc = Encoder(13, 19, enc_cb)
 
+  last_read = 0   # this keeps track of the last potentiometer value
+  tolerance = 550 # to keep from being jittery we'll only change
+                # volume when the pot has moved a significant amount
+                # on a 16-bit ADC
 
 
   #### others ####
@@ -406,77 +407,98 @@ def main():
 
   while True:
 
+    # we'll assume that the pot didn't move
+    trim_pot_changed = False
+    # read the analog pin
+    trim_pot = chan0.value
+    # how much has it changed since the last read?
+    pot_adjust = abs(trim_pot - last_read)
+
+
+    # counter
     if count_halfsecond() == True:
       simplecounter += 1
-      print(simplecounter)
+      #print(simplecounter)
 
-    ## checking if strip is in zone mode or default mode and determine the behaviour of the controls in both
+    if pot_adjust > tolerance:
+        trim_pot_changed = True
 
-    # if zone mode is OFF
-    if state_zonemode == 0:
+    if trim_pot_changed:
+        print(trim_pot)
+        if GPIO.input(SWITCH_BRIGHTNESS):
+          print("Brightness switch on")
+        elif GPIO.input(SWITCH_COLOR):
+          print("Color switch on")
+        elif not GPIO.input(SWITCH_BRIGHTNESS) and not GPIO.input(SWITCH_COLOR):
+          print("Saturation switch on")
 
-      # Knob behaviour
+        # save the potentiometer reading for the next loop
+        last_read = trim_pot
 
-      # if the color mode is off only temperature is adjusted
-      if state_colormode == 0 and ph_input_pot != ph_input_pot_prev:
-        strip.set_colortemp(map(ph_input_pot, (0.0, 1.0), (2500, 9000)), 0, False)
-        ph_input_pot_prev = ph_input_pot
 
-      # if the color mode is on, the 3-way switch selects which parameter is changed
-      else:
-        # if brightness mode
-        if not GPIO.input(SWITCH_BRIGHTNESS) and ph_input_pot != ph_input_pot_prev:
-          strip.set_brightness(65535 * ph_input_pot, 0, False)
-          ph_input_pot_prev = ph_input_pot
-        # if color mode
-        elif not GPIO.input(SWITCH_COLOR) and ph_input_pot != ph_input_pot_prev:
-          strip.set_hue(65535 * ph_input_pot, 0, False)
-          ph_input_pot_prev = ph_input_pot
-        # if saturation mode
-        elif GPIO.input(SWITCH_BRIGHTNESS) and GPIO.input(SWITCH_COLOR) and ph_input_pot != ph_input_pot_prev:
-          strip.set_saturation(65535 * ph_input_pot, 0, False)
-          ph_input_pot_prev = ph_input_pot
+    # if knob was turned
+    if trim_pot_changed:
 
-    else:
+        ## checking if strip is in zone mode or default mode and determine the behaviour of the controls in both
 
-      # Knob behaviour
+        # if zone mode is OFF
+        if state_zonemode == 0:
 
-      # if the color mode is off only temperature is adjusted
-      if state_colormode == 0 and ph_input_pot != ph_input_pot_prev:
-        zone_set_color[3] = map(ph_input_pot, (0.0, 1.0), (2500, 9000))
-        ph_input_pot_prev = ph_input_pot
+          # Knob behaviour
 
-      # if the color mode is on, the 3-way switch selects which parameter is changed
-      else:
-        # if brightness mode
-        if state_switch_brightness == 1 and ph_input_pot != ph_input_pot_prev:
-          zone_set_color[2] = 65535 * ph_input_pot
-          ph_input_pot_prev = ph_input_pot
-        # if color mode
-        elif state_switch_color == 1 and ph_input_pot != ph_input_pot_prev:
-          zone_set_color[0] = 65535 * ph_input_pot
-          ph_input_pot_prev = ph_input_pot
-        # if saturation mode
-        elif state_switch_brightness == 0 and state_switch_color == 0 and ph_input_pot != ph_input_pot_prev:
-          zone_set_color[1] = 65535 * ph_input_pot
-          ph_input_pot_prev = ph_input_pot
+          # if the color mode is off only temperature is adjusted
+          if state_colormode == 0:
+            strip.set_colortemp(map(trim_pot, (0, 65535), (2500, 9000)), 100, True)
 
-      # blinking behaviour
+          # if the color mode is on, the 3-way switch selects which parameter is changed
+          else:
+            # if brightness mode
+            if GPIO.input(SWITCH_BRIGHTNESS):
+              strip.set_brightness(trim_pot, 100, True)
+            # if color mode
+            elif GPIO.input(SWITCH_COLOR):
+              strip.set_hue(trim_pot, 100, True)
+            # if saturation mode
+            elif not GPIO.input(SWITCH_BRIGHTNESS) and not GPIO.input(SWITCH_COLOR):
+              strip.set_saturation(trim_pot, 100, True)
 
-      if simplecounter >= 2:
-        simplecounter = 0
-        temp_color = zone_set_color
-        temp_color[2] = 0
-        strip.set_zone_color(selected_zone, selected_zone, temp_color, 0, 1, 1)
-        print(temp_color)
-      if simplecounter == 1 and temp_color[2] == 0:
-        temp_color = zone_set_color
-        temp_color[2] = 65535
-        strip.set_zone_color(selected_zone, selected_zone, temp_color, 0, 1, 1)
-        #print(temp_color)
+        # if zone mode is ON
+        else:
 
-  # TODO preset button behaviour
+          # Knob behaviour
 
+          # if the color mode is off only temperature is adjusted
+          if state_colormode == 0:
+            zone_set_color[3] = map(trim_pot, (0, 65535), (2500, 9000))
+
+          # if the color mode is on, the 3-way switch selects which parameter is changed
+          else:
+            # if brightness mode
+            if state_switch_brightness == 1:
+              zone_set_color[2] = trim_pot
+            # if color mode
+            elif state_switch_color == 1:
+              zone_set_color[0] = trim_pot
+            # if saturation mode
+            elif state_switch_brightness == 0 and state_switch_color == 0:
+              zone_set_color[1] = trim_pot
+
+          # blinking behaviour
+
+          if simplecounter >= 2:
+            simplecounter = 0
+            temp_color = zone_set_color
+            temp_color[2] = 0
+            strip.set_zone_color(selected_zone, selected_zone, temp_color, 0, True, 1)
+            #print(temp_color)
+          if simplecounter == 1 and temp_color[2] == 0:
+            temp_color = zone_set_color
+            temp_color[2] = 65535
+            strip.set_zone_color(selected_zone, selected_zone, temp_color, 0, True, 1)
+            #print(temp_color)
+
+      # TODO preset button behaviour
+    sleep(0.1)
 
 if __name__=="__main__":
   main()
